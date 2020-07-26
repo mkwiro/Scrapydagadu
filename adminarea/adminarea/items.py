1b5dbb2625a63c965c923ab4234640d4f32c97c0
M K Wiro <muhammad.wiro@students.amikom.ac.id>
2020-07-26 09:59:32 +0700

    xpath adminarea

--- adminarea/adminarea/items.py
+++ adminarea/adminarea/items.py
@@ -8,5 +8,17 @@ import scrapy
 
 class AdminareaItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    barcode = scrapy.Field()
+    produk = scrapy.Field()
+    design = scrapy.Field()
+    ukuran = scrapy.Field()
+    warna = scrapy.Field()
+    sonosewu = scrapy.Field()
+    ytr = scrapy.Field()
+    posyandu = scrapy.Field()
+    sobo = scrapy.Field()
+    dgd_store = scrapy.Field()
+    jcm = scrapy.Field()
+    tugu = scrapy.Field()
+    total = scrapy.Field()
+    terjual = scrapy.Field()
