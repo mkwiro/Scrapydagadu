448049c9c437b6319aa90d27857081c135ba060e
M K Wiro <muhammad.wiro@students.amikom.ac.id>
2020-07-26 14:21:12 +0700

    scrap kategori dagadu di seluruh halaman

--- adminarea/adminarea/spiders/adminare_spider.py
+++ adminarea/adminarea/spiders/adminare_spider.py
@@ -1,12 +1,19 @@
 import scrapy
+import datetime
 from ..items import AdminareaItem
 
 
 class AdminSpider(scrapy.Spider):
     name ="admins"
+    page_number = 2
+    date = datetime.datetime.now()
+    b2 = ["00", "01", "03", "08", "09", "10", "14"]
     start_urls = [
-        'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php'
-    ]
+        'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari'
+        ]
+#    start_urls = [
+#        'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo=1'
+#    ]
 
     def parse(self, response):
 
@@ -49,3 +56,12 @@ class AdminSpider(scrapy.Spider):
 
 
             yield items
+
+            next_page = response.xpath('/html/body/div[1]/div[2]/div[2]/div[1]//li[12]/a/@href').get()
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
+
+
+            #next_page = 'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(AdminSpider.date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(AdminSpider.b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo='+ str(AdminSpider.page_number) +''
+            #if AdminSpider.page_number < 2:
+            #    AdminSpider.page_number += 1
