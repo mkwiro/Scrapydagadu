1a3c0674186b31845fb5d3b720e21e285d6216c4
M K Wiro <muhammad.wiro@students.amikom.ac.id>
2020-07-27 23:39:03 +0700

    5 jam scrap semua data dagadu

--- adminarea/adminarea/spiders/adminare_spider.py
+++ adminarea/adminarea/spiders/adminare_spider.py
@@ -8,12 +8,12 @@ class AdminSpider(scrapy.Spider):
     page_number = 2
     date = datetime.datetime.now()
     b2 = ["00", "01", "03", "08", "09", "10", "14"]
+    # start_urls = [
+    #     'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo=1'
+    #     ]
     start_urls = [
-        'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari'
-        ]
-#    start_urls = [
-#        'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo=1'
-#    ]
+       'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2=kosong&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari'
+   ]
 
     def parse(self, response):
 
@@ -62,6 +62,7 @@ class AdminSpider(scrapy.Spider):
                 yield response.follow(next_page, callback=self.parse)
 
 
-            #next_page = 'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(AdminSpider.date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(AdminSpider.b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo='+ str(AdminSpider.page_number) +''
-            #if AdminSpider.page_number < 2:
+            # next_page = 'http://103.27.36.114/sipandu2/ID_barang_perbandingan.php?date2='+ str(AdminSpider.date.strftime("%d-%m-%Y")) +'&katakunci=&b1=kosong&b2='+ str(AdminSpider.b2[1]) +'&b3=kosong&b11=kosong&b10=kosong&b4=kosong&b5=kosong&b6=kosong&b7=kosong&b8=kosong&txt_design=Semua+Design&b9=kosong&b12=kosong&PageSize=20&cHidden=1&cSubmit=Cari&PageNo='+ str(AdminSpider.page_number) +''
+            # if AdminSpider.page_number < 5:
             #    AdminSpider.page_number += 1
+            #    yield response.follow(next_page, callback=self.parse)
